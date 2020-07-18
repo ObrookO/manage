e112d45363ca1e9c7c1cccd147ee5de68191e539
xin <xin@xin.com>
2020-07-18 17:39:41 +0800

    修改一些细节

--- controllers/base.go
+++ controllers/base.go
@@ -75,8 +75,7 @@ func (c *BaseController) Prepare() {
 
 			// 判断权限
 			if ManagerInfo.IsAdmin != 1 {
-				if strings.Contains(path, "logs") || strings.Contains(path, "accounts") || strings.Contains(path,
-					"resource") || strings.Contains(path, "managers") {
+				if strings.Contains(path, "logs") || strings.Contains(path, "accounts") || strings.Contains(path, "managers") {
 					if c.IsAjax() {
 						c.Data["json"] = &JSONResponse{Code: 500, Msg: "非法访问"}
 						c.ServeJSON()
